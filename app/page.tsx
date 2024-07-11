75cbf642ea605994969e18335790ec93ce3277e7
ravisiyer <145967375+ravisiyer@users.noreply.github.com>
2024-07-11 23:50:10 +0530

    added hero images (desktop and mobile)

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,45 @@
 import ChapterSummaries from "./chaptersummaries";
 import { Suspense } from "react";
-import Link from "next/link";
+// import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <Suspense fallback={`Loading ...`}>
-      <ChapterSummaries />
-    </Suspense>
+    <>
+      <div>
+        <Image
+          src="/hero-desktop.jpg"
+          alt="Hero Image"
+          width={896}
+          height={538}
+          className="hidden md:block"
+          // Image aspect ratio is retained with width:896 and height:538.
+          // Hi-res image is 3040x1824. Compressed image is lesser.
+        />
+        <Image
+          src="/hero-mobile.jpg"
+          alt="Hero Image"
+          width={768}
+          height={888}
+          className="block md:hidden"
+          // Image aspect ratio is retained with width:768 and height:888.
+          // Hi-res image is 1578x1824. Compressed image is lesser.
+        />
+      </div>
+      <Suspense fallback={`Loading ...`}>
+        <ChapterSummaries />
+      </Suspense>
+      <hr className="border border-gray-400" />
+      <p className="text-xs text-right">
+        Top of page photo by{" "}
+        <a href="https://unsplash.com/@drone4inspection?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+          Oleg Churakov
+        </a>{" "}
+        on{" "}
+        <a href="https://unsplash.com/photos/a-statue-of-a-religious-figure-AKR89I3xf94?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+          Unsplash
+        </a>
+      </p>
+    </>
   );
 }
