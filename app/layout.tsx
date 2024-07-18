40e0b17d1051698ea867aa48ccfe15285143dcd3
ravisiyer <145967375+ravisiyer@users.noreply.github.com>
2024-07-18 20:15:17 +0530

    Added Next Js Toploader. Works for Links but not for replace (SCV)

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
 import type { Metadata } from "next";
 import Navbar from "./ui/navbar";
+import NextTopLoader from "nextjs-toploader";
 
 export const metadata: Metadata = {
   title: "Bhagavad Gita (Simple App)",
@@ -16,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased mx-2 mb-2 break-words`}>
+        <NextTopLoader color="rgb(251 146 60)" />
         <Navbar />
         <main className="mx-auto max-w-4xl p-2 scroll-mt-16 bg-yellow-100">
           {children}
