3fb80a51de3da4c7f71bf139abf18986873a115b
ravisiyer <145967375+ravisiyer@users.noreply.github.com>
2024-07-26 00:51:27 +0530

    cleaned up providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,19 +5,15 @@ import { ReactNode } from "react";
 export interface AppSettingsType {
   languageId: number;
 }
-// type LanguageIdType = number;
 export const AppSettingsContext = createContext<AppSettingsType>({
   languageId: 1,
 });
 
 function Providers({ children }: { children: ReactNode }) {
-  // const [languageId, setLanguageId] = useState<LanguageIdType>(1);
   const [languageId, setLanguageId] = useState(1);
   let AppSettings: AppSettingsType = { languageId: languageId };
-  // let  AppSettings:AppSettingsType = {languageId}
   return (
     <AppSettingsContext.Provider value={AppSettings}>
-      {/* <AppSettingsContext.Provider value={{ languageId, setLanguageId }}> */}
       {children}
     </AppSettingsContext.Provider>
   );
