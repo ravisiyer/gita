9caf877b9fa7e33046584a2c5a1bd578915a0362
ravisiyer <145967375+ravisiyer@users.noreply.github.com>
2024-08-30 21:04:36 +0530

    Updated About and README

--- README.md
+++ README.md
@@ -1,25 +1,39 @@
-**README update for [Version 1.3](https://github.com/ravisiyer/gita/releases/tag/v1.3)**
+This is a text only (without audio/video) Gita web app (Next.js, open source, freeware).
 
-This version adds to the Settings feature of earlier [version 1.2](https://github.com/ravisiyer/gita/releases/tag/v1.2), the ability of selecting translator for chapter page, use or not use the replace question mark (?) by comma (,) fix for commentaries data, selecting languages for chapter summaries and whether full window width should be used by all app pages for large window width devices like PC or laptop. It also uses higher resolution hero images (for home page).
+For its data, this Gita web app uses a public GraphQL endpoint: [https://gql.bhagavadgita.io/graphql](https://gql.bhagavadgita.io/graphql) ([GraphQL Explorer](https://gql.bhagavadgita.io/graphiql)), with possibly this [GitHub repository](https://github.com/gita/bhagavad-gita-graphql). I do not know how stable this data source is but for the past few months that I have been using it, it has been available all the time and been stable. This data source seems to be using a subset of data of [IIT Kanpur's Gita Super Site](https://www.gitasupersite.iitk.ac.in/srimad?language=dv&field_chapter_value=1&field_nsutra_value=1) , which publicly shares translations and commentaries of famous spiritual figures from India like [Swami Sivananda](https://en.wikipedia.org/wiki/Sivananda_Saraswati), [Swami Chinmayananda](https://en.wikipedia.org/wiki/Chinmayananda_Saraswati), [Swami Adidevananda](https://en.wikipedia.org/wiki/Swami_Adidevananda) and [Swami Gambirananda](https://en.wikipedia.org/wiki/Gambhirananda) . The Sanskrit commentaries of renowned ancient masters like Sri Shankaracharya, Sri Ramanujacharya and Sri Madhavacharya are also part of this data.
 
-**README update for [Version 1.2](https://github.com/ravisiyer/gita/releases/tag/v1.2)**
+This Gita web app is able to utilize the above-mentioned awesome GraphQL data resource to show translations and commentaries of above mentioned spiritual masters and more.
 
-This version adds to the Settings feature of earlier version 1.1, the ability of selecting individual translators and commentators within each language, whose translations and commentaries are shown in verse page. In version 1.1, only languages could be selected and all translations and commentaries of selected languages were shown in verse page.
+This Gita web app is deployed at: [https://gita-rsi.vercel.app/](https://gita-rsi.vercel.app/) and its Github public repository is: [https://github.com/ravisiyer/gita](https://github.com/ravisiyer/gita) . As it is a responsive web app, the app is accessible from Internet browsers (like Chrome/Edge/Safari) on PC desktop/laptop, Android mobile and Apple (iphone) mobile (and tablets though I have not tested it on tablets). Please note that it is NOT a native mobile (Android/iPhone) app.
 
-**README update for Version 1.1**
+Note that open source means that anybody is free to modify it and use it. Freeware means that anybody is free to use it in whatever way they want. I am using MIT software license for this purpose.
 
-This version adds to earlier [version 1.0](https://github.com/ravisiyer/gita/releases/tag/v1.0), the feature of user selecting the languages of translations and commentaries in verse page through a 'Settings' page.
+I think this software can be modified and used for any Chapter and Verse kind of application including, say, the Bible, Upanishads or a simple collection of poems. It could even be modified and used for Period (like year) and Discourse (like various dates in a year), kind of web app.
 
-**README of [Version 1.0](https://github.com/ravisiyer/gita/releases/tag/v1.0)**
+Adaptations of my Gita app for other needs like mentioned above, will need creation of suitable GraphQL database endpoints, or adaption of the app to use other database sources (like SQL (e.g. Postgres) or NoSQL (e.g. MongoDB) databases).
 
-This Gita web app has been developed on top of [Very Simple Gita](https://github.com/ravisiyer/verysimplegita) which is a very simple but functional, open-source freeware, Bhagavad Gita web app.
+Web app version: 1.4, Date: 30th August 2024
 
-I have changed the User Interface (UI) of this app to a regular web app UI. But, from a source code point of view, it no longer is a very simple Gita app. Students and other learners may want to first go through the source code of Very Simple Gita app before going through this app's source code.
+Web app author: Ravi S. Iyer
 
-This Gita web app uses Next.js and Apollo Client to retrieve Bhagavad Gita chapters and verses data from a GraphQL endpoint: https://gql.bhagavadgita.io/graphql (Explorer: https://gql.bhagavadgita.io/graphiql), and displays it on browser. It also uses Tailwind CSS and TypeScript.
+**Footnotes**
 
-This Gita web app is open-source and shared under free-software MIT license.
+1. This Gita web app uses Next.js and Apollo Client to retrieve Bhagavad Gita chapters and verses data from a GraphQL endpoint, and displays it on browser. It also uses Tailwind CSS and TypeScript.
 
-Note that there seems to be a problem with the data associated with commentaries of Swami Sivananda and some others. Question mark character (?) appears frequently in places where I think there should be a comma character (,). I have raised the [issue on the GitHub repository associated with the GraphQL endpoint](https://github.com/gita/bhagavad-gita-graphql/issues/2). As a temporary measure (hack), in the Verse page which is the only page which shows commentaries, I am replacing ? character with , character in all commentaries data retrieved from the data source. The disadvantage is that even ? characters that should be ? will be changed to , characters. But such cases may be very few.
+2. There seems to be a question mark character problem with the data associated with some commentaries for which I am providing a temporary fix in the app. More info on it is provided in a page mentioned in About page of the app.
 
-For more about this program, please visit [Gita Web App v1.0 (Open Source), Pathway to Self-Learn Next.js Web Dev with Tailwind CSS and TypeScript](https://raviswdev.blogspot.com/2024/07/gita-app-v10-pathway-to-self-learn.html).
+3. An important objective of this app development has been to provide a pathway to self-learner students to follow the path I used to learn the required technologies and develop this app. I have made extensive notes about the path I used in the blog some of whose posts are mentioned below.
+
+4. Some more info. about this web app can be read in the following blog posts, one for each release version of this app:
+
+   [Version 1.4 (this version)](https://raviswdev.blogspot.com/2024/08/gita-web-app-nextjs-open-source-v14.html)
+
+   [Version 1.3](https://raviswdev.blogspot.com/2024/08/gita-web-app-nextjs-open-source-v13.html)
+
+   [Version 1.2](https://raviswdev.blogspot.com/2024/08/gita-web-app-nextjs-open-source-v12.html)
+
+   [Version 1.1](https://raviswdev.blogspot.com/2024/08/added-settings-page-to-gita-web-app-to.html)
+
+   [Version 1.0](https://raviswdev.blogspot.com/2024/07/gita-app-v10-pathway-to-self-learn.html)
+
+5. This Gita web app has been developed on top of [Very Simple Gita](https://github.com/ravisiyer/verysimplegita) which is a very simple but functional, open-source freeware, Bhagavad Gita web app. I have changed the User Interface (UI) of this app to a regular web app UI. But, from a source code point of view, it no longer is a very simple Gita app. Students and other learners may want to first go through the source code of Very Simple Gita app before going through this app's source code.
--- app/(mainapp)/about/page.tsx
+++ app/(mainapp)/about/page.tsx
@@ -11,33 +11,77 @@ function Page() {
     <div>
       <h2 className="my-5 text-2xl font-bold">About Web App & Data</h2>
       <p className="my-4">
-        This Gita web app has been developed on top of{" "}
+        This is a text only (without audio/video) Gita web app (Next.js, open
+        source, freeware).
+      </p>
+      <p className="my-4">
+        For its data, this Gita web app uses a public GraphQL endpoint:{" "}
+        {GRAPHQL_URI} (GraphQL Explorer:{" "}
         <a
-          href="https://github.com/ravisiyer/verysimplegita"
+          href={GRAPHQL_EXPLORER_URI}
           className="text-blue-700 visited:text-purple-900 underline"
         >
-          Very Simple Gita
+          {GRAPHQL_EXPLORER_URI}
+        </a>
+        ), with possibly this{" "}
+        <a
+          href="https://github.com/gita/bhagavad-gita-graphql"
+          className="text-blue-700 visited:text-purple-900 underline"
+        >
+          GitHub repository
+        </a>
+        . I do not know how stable this data source is but for the past few
+        months that I have been using it, it has been available all the time and
+        been stable. This data source seems to be using a subset of data of{" "}
+        <a
+          href="https://www.gitasupersite.iitk.ac.in/srimad?language=dv&field_chapter_value=1&field_nsutra_value=1"
+          className="text-blue-700 visited:text-purple-900 underline"
+        >
+          IIT Kanpur's Gita Super Site
+        </a>
+        , which publicly shares translations and commentaries of famous
+        spiritual figures from India like{" "}
+        <a
+          href="https://en.wikipedia.org/wiki/Sivananda_Saraswati"
+          className="text-blue-700 visited:text-purple-900 underline"
+        >
+          Swami Sivananda
+        </a>
+        ,{" "}
+        <a
+          href="https://en.wikipedia.org/wiki/Chinmayananda_Saraswati"
+          className="text-blue-700 visited:text-purple-900 underline"
+        >
+          Swami Chinmayananda
+        </a>
+        ,{" "}
+        <a
+          href="https://en.wikipedia.org/wiki/Swami_Adidevananda"
+          className="text-blue-700 visited:text-purple-900 underline"
+        >
+          Swami Adidevananda
         </a>{" "}
-        which is a very simple but functional, open-source freeware, Bhagavad
-        Gita web app.
+        and{" "}
+        <a
+          href="https://en.wikipedia.org/wiki/Gambhirananda"
+          className="text-blue-700 visited:text-purple-900 underline"
+        >
+          Swami Gambirananda
+        </a>
+        . The Sanskrit commentaries of renowned ancient masters like Sri
+        Shankaracharya, Sri Ramanujacharya and Sri Madhavacharya are also part
+        of this data.
       </p>
       <p className="my-4">
-        I have changed the User Interface (UI) of this app to a regular web app
-        UI. But, from a source code point of view, it no longer is a very simple
-        Gita app. Students and other learners may want to first go through the
-        source code of Very Simple Gita app before going through this app&apos;s
-        source code.
+        This Gita web app is able to utilize the above-mentioned awesome GraphQL
+        data resource to show translations and commentaries of above mentioned
+        spiritual masters and more. As it is a responsive web app, the app is
+        accessible from Internet browsers (like Chrome/Edge/Safari) on PC
+        desktop/laptop, Android mobile and Apple (iphone) mobile (and tablets
+        though I have not tested it on tablets). Please note that it is NOT a
+        native mobile (Android/iPhone) app.
       </p>
-      <p className="my-4">
-        This Gita web app uses Next.js and Apollo Client to retrieve Bhagavad
-        Gita chapters and verses data from a GraphQL endpoint, and displays it
-        on browser. It also uses Tailwind CSS and TypeScript.
-      </p>
-      <p className="my-4">
-        This Gita web app is open-source and shared under free-software MIT
-        license.
-      </p>
-      <p className="my-4">
+      {/* <p className="my-4">
         Data source (GraphQL endpoint): {GRAPHQL_URI} (GraphQL Explorer:{" "}
         <a
           href={GRAPHQL_EXPLORER_URI}
@@ -46,18 +90,8 @@ function Page() {
           {GRAPHQL_EXPLORER_URI}
         </a>
         )
-      </p>
-      <p className="my-4">
-        Note that there seems to be a question mark character problem with the
-        data associated with some commentaries.{" "}
-        <Link
-          href="/qmarkissue"
-          className="text-blue-700 visited:text-purple-900 underline"
-        >
-          See more info on it.
-        </Link>
-      </p>
-      <p className="my-4">
+      </p> */}
+      {/* <p className="my-4">
         More about this web app can be read in{" "}
         <a
           href="https://raviswdev.blogspot.com/2024/07/gita-app-v10-pathway-to-self-learn.html"
@@ -66,7 +100,7 @@ function Page() {
           this associated blog post
         </a>
         .
-      </p>
+      </p> */}
       <p className="my-4">
         App GitHub{" "}
         <a
@@ -82,13 +116,102 @@ function Page() {
         >
           README
         </a>
-        .
+        . This README provides more details about the app.
       </p>
       <p className="mt-4">
-        Web app version: Post version 1.3, Date: 28th August 2024
+        Web app version: 1.4, Date: 30th August 2024
         <br />
         Web app author: Ravi S. Iyer
       </p>
+      {/* <h3 className="text-lg font-bold mt-2">Footnotes</h3>
+      <ol className="list-decimal list-outside ml-4">
+        <li>
+          This Gita web app uses Next.js and Apollo Client to retrieve Bhagavad
+          Gita chapters and verses data from a GraphQL endpoint, and displays it
+          on browser. It also uses Tailwind CSS and TypeScript.
+        </li>
+        <li>
+          There seems to be a question mark character problem with the data
+          associated with some commentaries for which I am providing a temporary
+          fix in the app.{" "}
+          <Link
+            href="/qmarkissue"
+            className="text-blue-700 visited:text-purple-900 underline"
+          >
+            See more info on it.
+          </Link>
+        </li>
+        <li>
+          An important objective of this app development has been to provide a
+          pathway to self-learner students to follow the path I used to learn
+          the required technologies and develop this app. I have made extensive
+          notes about the path I used in the blog some of whose posts are
+          mentioned below.
+        </li>
+        <li>
+          Some more info. about this web app can be read in the following blog
+          posts, one for each release version of this app:
+          <ul>
+            <li>
+              <a
+                href="https://raviswdev.blogspot.com/2024/08/gita-web-app-nextjs-open-source-v14.html"
+                className="text-blue-700 visited:text-purple-900 underline"
+              >
+                Version 1.4 (this version)
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://raviswdev.blogspot.com/2024/08/gita-web-app-nextjs-open-source-v13.html"
+                className="text-blue-700 visited:text-purple-900 underline"
+              >
+                Version 1.3
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://raviswdev.blogspot.com/2024/08/gita-web-app-nextjs-open-source-v12.html"
+                className="text-blue-700 visited:text-purple-900 underline"
+              >
+                Version 1.2
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://raviswdev.blogspot.com/2024/08/added-settings-page-to-gita-web-app-to.html"
+                className="text-blue-700 visited:text-purple-900 underline"
+              >
+                Version 1.1
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://raviswdev.blogspot.com/2024/07/gita-app-v10-pathway-to-self-learn.html"
+                className="text-blue-700 visited:text-purple-900 underline"
+              >
+                Version 1.0
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <p className="">
+            This Gita web app has been developed on top of{" "}
+            <a
+              href="https://github.com/ravisiyer/verysimplegita"
+              className="text-blue-700 visited:text-purple-900 underline"
+            >
+              Very Simple Gita
+            </a>{" "}
+            which is a very simple but functional, open-source freeware,
+            Bhagavad Gita web app. I have changed the User Interface (UI) of
+            this app to a regular web app UI. But, from a source code point of
+            view, it no longer is a very simple Gita app. Students and other
+            learners may want to first go through the source code of Very Simple
+            Gita app before going through this app&apos;s source code.
+          </p>
+        </li>
+      </ol> */}
     </div>
   );
 }
